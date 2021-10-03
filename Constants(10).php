ae357e21eabcb2227b9a27c9dbf19cfec765155a
Ishtiaque1997 <60317006+Ishtiaque1997@users.noreply.github.com>
2021-10-03 13:45:20 +0600

    define-done

---
+++ Constants(10).php
@@ -0,0 +1,12 @@
+<html>
+<head>
+<h1>First program</h1>
+</head>
+<body>
+<?php
+//const means putting something in another
+  define("Learn",'Learn hunter coming');
+  echo Learn ;
+?>
+</body>
+</html>

d002687b804134b144fbfdf11c4c6b79ee1c673c
Ishtiaque1997 <60317006+Ishtiaque1997@users.noreply.github.com>
2021-10-03 19:32:25 +0600

    condition done

---
+++ Increment-Decrement-operator(15).php
@@ -0,0 +1,20 @@
+<html>
+<head>
+<h1>First program</h1>
+</head>
+<body>
+<?php
+  //increment and decrement operator
+  //++$x  $x++  --$x  $x--
+  $number=56;
+  echo ++$number;//increases by 1 before
+  echo '<br>';
+  echo --$number;//decreases by 1 before
+  echo '<br>';
+  echo $number++;//increases by 1 later
+  echo '<br>';
+  echo $number;
+  
+?>
+</body>
+</html>
---
+++ Logical-operator(16).php
@@ -0,0 +1,25 @@
+<html>
+<head>
+<h1>First program</h1>
+</head>
+<body>
+<?php
+  $num=10;
+  if($num==109){
+   echo 'doing well';
+  }
+  else if($num==20){
+	 echo'second done';
+  }
+   else if($num==90){
+	 echo'third done';
+  }
+   else if($num==19090){
+	 echo'yess done';
+  }
+  else{
+  echo'not ok';
+  }
+?>
+</body>
+</html>

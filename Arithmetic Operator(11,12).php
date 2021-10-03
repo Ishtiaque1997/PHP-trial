51a55126480e1b073569dcab00304d1ccdc4333a
Ishtiaque1997 <60317006+Ishtiaque1997@users.noreply.github.com>
2021-10-03 17:52:16 +0600

    arithmetic operator done

---
+++ Arithmetic Operator(11,12).php
@@ -0,0 +1,31 @@
+<html>
+<head>
+
+</head>
+<body>
+<?php
+//operators
+//1. Arithmetic 2. Assignment 3. Increment/Decrement 4. Logical 5. Comparison 6. String 7. Array
+//Arithmetic operator => =,-,*,/,%,**
+$a=10;
+$b=2;
+$sum= $a+$b;//add
+echo $sum;
+echo '<br>';
+$diff=$a-$b;//subtract
+echo $diff;
+echo '<br>';
+$pd=$a*$b;//muttiply
+echo $pd;
+echo '<br>';
+$q=$a/$b;//divide
+echo $q;
+echo '<br>';
+$mod=$a%$b;//modulus
+echo $mod;
+echo '<br>';
+$double=$a**$b;//power a^b;
+echo $double
+?>
+</body>
+</html>

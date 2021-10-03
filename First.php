c7c91b486d178d78bd780bb681c0c6a1756ae7da
Ishtiaque1997 <60317006+Ishtiaque1997@users.noreply.github.com>
2021-10-03 13:12:40 +0600

    var-more-done

--- First.php
+++ First.php
@@ -4,14 +4,26 @@
 </head>
 <body>
 <?php
-  
+  //declare a variable
   $name='sakib';
   $number=89;
-  $fact=true;
+  $fact=false;
+  
+  //print a variable
   echo $name;
- 
+  echo "<br>";//goes to next line
   echo $number;
-
+  echo $fact;
+  
+  //echo is faster and takes more parameter but print is slow and can take only one
+  echo "hello world,'iopiop'";
+  echo "<br>";
+  print"hello world";
+  echo "<br>";
+  
+  //var_dump gives length, type of a variable
+  var_dump($name);
+  //print_r
 ?>
 </body>
 

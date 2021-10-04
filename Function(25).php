a64d406431ec00746b4fd6c707583704ab738f0e
Ishtiaque1997 <60317006+Ishtiaque1997@users.noreply.github.com>
2021-10-04 16:36:28 +0600

    function going

---
+++ Function(25).php
@@ -0,0 +1,18 @@
+<html>
+<head>
+<h1>First program</h1>
+</head>
+<body>
+<?php
+  function myIdentity($name,$age){
+     //echo 'learn hunter(youtube)';
+     echo "My name is :$name and age is :$age" ;
+     
+
+  }
+  myIdentity("Ishtiaque",'24');
+  
+  
+?>
+</body>
+</html>

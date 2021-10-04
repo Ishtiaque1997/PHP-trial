1d4fcb6803ea29f890422b4edfc196c68860b4d5
Ishtiaque1997 <60317006+Ishtiaque1997@users.noreply.github.com>
2021-10-04 15:25:07 +0600

    loop-going

---
+++ Do-whilee(22).php
@@ -0,0 +1,17 @@
+<html>
+<head>
+<h1>First program</h1>
+</head>
+<body>
+<?php
+//do while loop executes first and then check condition
+ $num=1;
+ do{
+  echo "number is : $num <br>";
+  $num++;
+ }
+ while($num<10);
+
+?>
+</body>
+</html>
---
+++ While-Loop(21).php
@@ -0,0 +1,17 @@
+<html>
+<head>
+<h1>First program</h1>
+</head>
+<body>
+<?php
+ //for,while,for each,do while
+ //while loop checks condition and then executes
+ $a=10;
+while($a<=100){
+  echo "number is: $a <br>";
+  $a++;
+}
+
+?>
+</body>
+</html>

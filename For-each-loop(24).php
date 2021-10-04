941e948503eb61d49616d08be48384186c16f2c2
Ishtiaque1997 <60317006+Ishtiaque1997@users.noreply.github.com>
2021-10-04 16:22:12 +0600

    loop-done

---
+++ For-each-loop(24).php
@@ -0,0 +1,15 @@
+<html>
+<head>
+<h1>First program</h1>
+</head>
+<body>
+<?php
+  //for each loop used to get data from array
+  $names=array('sohel','rana','sakib','sojib','rina');
+  foreach ($names as $name) {
+   echo "$name <br>";
+  }
+  
+?>
+</body>
+</html>
